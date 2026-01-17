e6df090ccfd2a5c5dcf705b239e578665a258321
likkiLikitha <likithalikki047@gmail.com>
2026-01-17 20:34:18 +0530

    Add divide functionality

--- calculator.py
+++ calculator.py
@@ -4,5 +4,12 @@ def add(a, b):
 def multiply(a, b):
     return a * b
 
+<<<<<<< HEAD
 def subtract(a, b):
     return a - b
+=======
+def divide(a, b):
+    if b == 0:
+        return None
+    return a / b
+>>>>>>> 779c37d (Add divide functionality)
