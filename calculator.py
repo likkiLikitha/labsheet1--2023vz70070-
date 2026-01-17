16375f522e4c28e5cdd3855587356afd7076c218
likkiLikitha <likithalikki047@gmail.com>
2026-01-17 20:54:18 +0530

    Update calculator with subtract and divide functions

--- calculator.py
+++ calculator.py
@@ -4,12 +4,10 @@ def add(a, b):
 def multiply(a, b):
     return a * b
 
-<<<<<<< HEAD
 def subtract(a, b):
     return a - b
-=======
+
 def divide(a, b):
     if b == 0:
         return None
     return a / b
->>>>>>> 779c37d (Add divide functionality)
